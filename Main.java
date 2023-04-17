5e500034f59ab899e39f0bd918c239072dc1369f
JoySoon <115054565+JoySoon@users.noreply.github.com>
2023-04-17 03:47:30 +0000

    Hello Java

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ class Main {
   public static void main(String[] args) {
     System.out.println("Hello playdata!");
     System.out.println("Hello Hello!");
+    System.out.println("Hello Java!");
   }
 }
